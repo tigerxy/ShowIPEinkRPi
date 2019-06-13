2474cf518a3f691c967bbbc623db1a50d6f16c76
Roland Greim <roland@stempelgreim.de>
2019-06-13 18:04:09 +0200

    Added date and time. fixes #3

--- displayIPAddress.py
+++ displayIPAddress.py
@@ -6,12 +6,14 @@ import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 import socket
+import datetime
 
 try:
 	epd = epd2in13.EPD()
 
 	host_name = socket.gethostname() 
-	host_ip = socket.gethostbyname(host_name + ".local") 
+	host_ip = socket.gethostbyname(host_name + ".local")
+	date_time = str(datetime.datetime.now())[:19] 
 
 	epd.init(epd.lut_full_update)
 
@@ -20,12 +22,13 @@ try:
 	except:
 		image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
 	font24 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 24)
+	font18 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 18)
 	fontbold24 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
 	draw = ImageDraw.Draw(image)
-	draw.text((10, 10), "Hostname:", font = font24, fill = 0)
+	draw.text((10, 10), "Hostname/IP:", font = font24, fill = 0)
 	draw.text((10, 35), host_name, font = fontbold24, fill = 0)
-	draw.text((10, 65), "IP-Address:", font = font24, fill = 0)
-	draw.text((10, 90), host_ip, font = fontbold24, fill = 0)
+	draw.text((10, 65), host_ip, font = fontbold24, fill = 0)
+	draw.text((10, 95), date_time, font = font18, fill = 0)
 	epd.display(epd.getbuffer(image.rotate(180)))
 		
 	epd.sleep()
