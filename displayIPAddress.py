9a0b41e7289979d17b90c5d6feed1ab91f0ded11
Roland Greim <roland@stempelgreim.de>
2019-06-13 19:07:48 +0200

    Waiting for IP-Address. Dirty solution for #2

--- displayIPAddress.py
+++ displayIPAddress.py
@@ -8,15 +8,31 @@ import traceback
 import socket
 import datetime
 
+def tryGetIPAddress():
+	try:
+		return socket.gethostbyname(socket.gethostname() + ".local")
+	except:
+		return 0
+
 try:
 	epd = epd2in13.EPD()
+	epd.init(epd.lut_partial_update)
 
+	# Waiting for IP-Address
+	if not tryGetIPAddress():
+		# Drawing a blank display
+		epd.Clear(255)
+		epd.sleep()
+	while not tryGetIPAddress():
+		time.sleep(1)
+
+	# Collect informations
 	host_name = socket.gethostname() 
-	host_ip = socket.gethostbyname(host_name + ".local")
+	host_ip = tryGetIPAddress()
 	date_time = str(datetime.datetime.now())[:19] 
 
+	# Drawing informations on diplay
 	epd.init(epd.lut_full_update)
-
 	try:
 		image = Image.open('/home/pi/.config/autostart/train.bmp') 
 	except:
@@ -24,13 +40,13 @@ try:
 	font24 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 24)
 	font18 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 18)
 	fontbold24 = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+	#epd.display(epd.getbuffer(image.rotate(180)))
 	draw = ImageDraw.Draw(image)
 	draw.text((10, 10), "Hostname/IP:", font = font24, fill = 0)
 	draw.text((10, 35), host_name, font = fontbold24, fill = 0)
 	draw.text((10, 65), host_ip, font = fontbold24, fill = 0)
 	draw.text((10, 95), date_time, font = font18, fill = 0)
 	epd.display(epd.getbuffer(image.rotate(180)))
-		
 	epd.sleep()
         
 except:
